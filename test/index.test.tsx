84e05bcc9b9dd98a8f793918b984e5391c1a22c1
Peerapong Maitriwong <work@peerapong.dev>
2021-03-30 13:14:33 +0700

    fix: next build error

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 
-import Home from "./index";
+import Home from "../pages/index";
 
 describe("Home page", () => {
   it("display title correctly", () => {
