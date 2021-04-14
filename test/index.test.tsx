8147a54ae6e83679726de809c3376a4e8dcf3086
Peerapong Maitriwong <work@peerapong.dev>
2021-04-14 15:59:40 +0700

    fix: lint fail on testing-library

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,11 +1,11 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Home from "../pages/index";
 
 describe("Home page", () => {
   it("display title correctly", () => {
-    const { getByTestId } = render(<Home />);
+    render(<Home />);
 
-    expect(getByTestId("title")).toHaveTextContent(/Next.js!/i);
+    expect(screen.getByTestId("title")).toHaveTextContent(/Next.js!/i);
   });
 });
